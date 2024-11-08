863dfd7c2d40f5044438efc2e964360016355253
D4vsus <davsus05@gmail.com>
2024-11-08 19:19:07 +0100

    Improved the project

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,8 +6,20 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
+/**
+ * <h1>Main</h1>
+ * <p>Initialize the application</p>
+ * 
+ * @author D4vsus
+ */
 public class Main extends Application {
+	
+	//methods
+	
+	/**
+	 * <h1>start()</h1>
+	 * <p>Initialize the application</p>
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -16,6 +28,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Weka example");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -3,9 +3,6 @@ package application;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -17,9 +14,26 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 
+/**
+ * <h1>MainWindowController</h1>
+ * <p>Add functionality to the window</p>
+ * 
+ * @author D4vsus
+ */
 public class MainWindowController {
+	//variables and objects
 	private static Classifier algorithm;
-
+	@FXML
+	private TextArea inputArea;
+	@FXML
+	private Label feedBack;
+	
+	//methods
+	
+	/**
+	 * <h1>initAlgorithm()</h1>
+	 * <p>Load the algorithm file from the resources</p>
+	 */
 	public static void initAlgorithm() {
 		try {
 			algorithm = (Classifier) SerializationHelper
@@ -38,41 +52,60 @@ public class MainWindowController {
 		}
 
 	}
-
-	@FXML
-	private TextArea inputArea;
-	@FXML
-	private Label feedBack;
-
+	
+	/**
+	 * <h1>predict()</h1>
+	 * <p>Make a prediction each time the user types in the keyboard</p>
+	 */
 	@FXML
 	protected void predict() {
 		try {
+			//create the values for the label field
 			ArrayList<String> labelValues = new ArrayList<String>();
 			labelValues.add("POSITIVE");
 			labelValues.add("NEGATIVE");
 
+			//create the attributes
 			ArrayList<Attribute> attributes = new ArrayList<Attribute>();
 			attributes.add(new Attribute("Review", (ArrayList<String>) null)); // String attribute
 			attributes.add(new Attribute("label", labelValues)); // Nominal attribute for label
 
+			//create the dataset
 			Instances instances = new Instances("Predictions", attributes, 1);
 			instances.setClassIndex(1); // Set the class attribute index
 
-			// Create a new instance and set the dataset for the instance
+			//create the instance for the dataset
 			Instance instance = new DenseInstance(2);
-			instance.setDataset(instances); // Important: Set dataset for instance
-
-			instance.setValue(attributes.get(0), inputArea.getText()); // Set "Review" text
-			// No need to set label value for prediction (unlabeled instance)
-
-			instances.add(instance); // Add the instance to the dataset
-
-			// Classify the instance
-			double prediction = algorithm.classifyInstance(instance);
-			if (prediction == 1.0) {
-				feedBack.setText("NEGATIVE");
-			} else if (prediction == 0.0) {
+			instance.setDataset(instances);
+			instance.setValue(attributes.get(0), inputArea.getText()); // set the text of the text field as the Review column
+			instances.add(instance); 
+			
+			//make the prediction
+			double[] results = algorithm.distributionForInstance(instance);
+			int prediction = -1; //if the prediction don't have enough confidence, by default will be one
+			double mostConfidentClass = 0;
+			int resultSize = results.length;
+			
+			//get the index of the most confident result
+			for (int i = 0; i < resultSize; i++) {
+				//if the most confident value is lower than 65% ignore and if is more confident than the most confident class in the moment
+				if (results[i] > 0.65 && results[i] > mostConfidentClass) {
+					mostConfidentClass = results[i]; // set the new most confident class;
+					prediction = i; // assign it to the prediction
+				}
+			}
+			
+			//output the instance
+			switch (prediction) {
+			case 0:
 				feedBack.setText("POSITIVE");
+				break;
+			case 1:
+				feedBack.setText("NEGATIVE");
+				break;
+			default:
+				feedBack.setText("?");
+				break;
 			}
 
 		} catch (FileNotFoundException e) {
